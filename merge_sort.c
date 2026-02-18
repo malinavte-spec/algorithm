15dad8e1cdf8e6bd36f803ab47f46106a28ad149
Malina <malina@Malinas-MacBook-Pro.local>
2026-02-18 15:56:48 +0800

    initial commit

---
+++ merge_sort.c
@@ -0,0 +1,24 @@
+// MergeSort(A):
+// 1. If the length of A is less than or equal to 1,
+//       return A   // already sorted
+
+// 2. Find the middle index of A
+//       mid = n / 2
+
+// 3. Divide A into two halves:
+//       Left = A[0 ... mid-1]
+//       Right = A[mid ... n-1]
+
+// 4. Recursively call:
+//       MergeSort(Left)
+//       MergeSort(Right)
+
+// 5. Merge the sorted halves:
+//       return Merge(Left, Right)
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
